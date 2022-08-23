7124408d164eadff1b6a179355194356a10a14d3
Manoel Duran <manoel.duran@hotmail.com>
2022-08-23 11:13:03 -0300

    created mvpsfirstroom  repository

---
+++ src/modules/MvpsFirstRoom/repositories/implementations/MvpsFirstRoomRepository.ts
@@ -0,0 +1,27 @@
+import { getRepository, Repository } from "typeorm";
+import { MvpsFirstRoom } from "../../entities/MvpsFirstRoom";
+import { ICreateMvpsFirstRoomDTO, IMvpsFirstRoomRepository } from "../IMvpsFirstRoomRepository";
+
+
+class MvpsFirstRoomRepository implements IMvpsFirstRoomRepository {
+    private repository: Repository<MvpsFirstRoom>
+    constructor() {
+        this.repository = getRepository(MvpsFirstRoom)
+    };
+    async create({ image, breed, dark, earth, fire, ghost, holy, level, name, neutral, poison, property, quantity, undead, water, wind }: ICreateMvpsFirstRoomDTO): Promise<void> {
+        const mvpFirstRoom = this.repository.create({
+            image, breed, dark, earth, fire, ghost, holy, level, name, neutral, poison, property, quantity, undead, water, wind
+        });
+        await this.repository.save(mvpFirstRoom);
+    }
+    async findByName(name: string): Promise<MvpsFirstRoom> {
+        const alreadyExists = await this.repository.findOne({ name });
+        return alreadyExists;
+    }
+    async list(): Promise<MvpsFirstRoom[]> {
+        const mvpsFirstRoom = this.repository.find();
+        return mvpsFirstRoom;
+    }
+}
+
+export { MvppsFirstRoomRepository };
