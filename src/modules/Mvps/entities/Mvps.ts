09e41694ef0def0f425d66dd55df32264fdef7af
Manoel Duran <manoel.duran@hotmail.com>
2022-10-03 11:19:06 -0300

    feat(Mvps): created Mvps entity

---
+++ src/modules/Mvps/entities/Mvps.ts
@@ -0,0 +1,57 @@
+import { v4 as uuidV4 } from 'uuid';
+import { Column, Entity, PrimaryColumn } from 'typeorm';
+
+@Entity("mvps")
+class Mvps {
+    @PrimaryColumn()
+    id?: string;
+
+    @Column()
+    name: string;
+
+    @Column()
+    quantity: number;
+
+    @Column()
+    image?: string;
+
+    @Column()
+    property: string;
+
+    @Column()
+    respawn: string;
+
+    @Column()
+    breed: string;
+
+    @Column()
+    level: number;
+    @Column()
+    neutral: number;
+    @Column()
+    water: number;
+    @Column()
+    earth: number;
+    @Column()
+    fire: number;
+    @Column()
+    wind: number;
+    @Column()
+    poison: number;
+    @Column()
+    holy: number;
+    @Column()
+    dark: number;
+    @Column()
+    ghost: number;
+    @Column()
+    undead: number;
+    constructor() {
+        if (!this.id) {
+            this.id = uuidV4();
+        };
+    };
+
+};
+
+export { Mvps };
