664af110ac002c96d13162dfe629b8188ba7b6bf
Manoel Duran <manoel.duran@hotmail.com>
2023-03-21 10:18:49 -0300

    fixed  serverless run migrations

--- migration.ts
+++ migration.ts
@@ -1,6 +1,13 @@
 'use strict';
 import { Connection, createConnection, getConnectionOptions } from 'typeorm';
-import   './src/shared/infra/typeorm/migrations/1663352042618-CreateUsers'
+import './src/shared/infra/typeorm/migrations/1663352042618-CreateUsers'
+import './src/shared/infra/typeorm/migrations/1668176864432-CreateMvp'
+import './src/shared/infra/typeorm/migrations/1671412755427-CreateRoom'
+import './src/shared/infra/typeorm/migrations/1671461949794-CreateMap'
+import './src/shared/infra/typeorm/migrations/1671575424950-CreateTimer'
+import './src/shared/infra/typeorm/migrations/1672000477656-CreateNotification'
+import './src/shared/infra/typeorm/migrations/1672263876772-CreateRoomMvp'
+import './src/shared/infra/typeorm/migrations/1677675321194-AddIsGeneralToMvp'
 
 
 
@@ -20,6 +27,7 @@ module.exports.handler = async () => {
         }
     });
 
+     await connection.dropDatabase()
     const migrations = await connection.runMigrations();
-    return { migrations};
+    return { migrations };
 };
